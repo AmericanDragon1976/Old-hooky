2c83026e47c912a9d7c75f779154ebda2368c517
Tyler Flint <tylerflint@gmail.com>
2013-06-27 10:53:35 -0600

    adding a simple echo client test

---
+++ test/echo_client.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby -wKU
+
+require 'socket'
+
+class Connection
+
+  MAX_ATTEMPTS = 30
+
+  attr_accessor :socket
+  
+  def initialize(host='127.0.0.1', port=3318, autoconnect=true)
+    @host     = host
+    @port     = port
+    @attempts = 0
+    connect if autoconnect
+  end
+
+  def deliver(message)
+    socket.print [message.bytes.count].pack('N') # 32-bit unsigned big-endian
+    socket.print message
+    # puts "deliver: #{message}"
+    socket.flush
+  end
+
+  def receive
+    len  = socket.recv(4).unpack('N')[0] # 32-bit unsigned, big-endian
+    data = ''
+    until data.bytes.count == len do
+      data += socket.recv (len - data.bytes.count)
+    end
+      # puts "receive: #{data}"
+    data
+  end
+
+  def socket
+    @socket ||= establish_connection
+  end
+
+  alias :connect :socket
+
+  def establish_connection
+    TCPSocket.open @host, @port
+  end
+end
+
+host = '127.0.0.1'
+port = '10000'
+
+connection = Connection.new host, port
+
+connection.deliver "hello world!"
+
+puts connection.receive
