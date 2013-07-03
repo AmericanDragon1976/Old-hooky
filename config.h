374f55c8cbb4d978975b9c2b930742b7e27dfb50
Edward Moroni Goodson <bobby_83110@yahoo.com>
2013-07-03 11:02:47 -0600

    added other files

---
+++ config.h
@@ -0,0 +1,38 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `crypto' library (-lcrypto). */
+#define HAVE_LIBCRYPTO 1
+
+/* Define to 1 if you have the `event' library (-levent). */
+#define HAVE_LIBEVENT 1
+
+/* Define to 1 if you have the `event_openssl' library (-levent_openssl). */
+#define HAVE_LIBEVENT_OPENSSL 1
+
+/* Define to 1 if you have the `json-c' library (-ljson-c). */
+#define HAVE_LIBJSON_C 1
+
+/* Name of package */
+#define PACKAGE "hooky"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "hooky"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "hooky 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "hooky"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Version number of package */
+#define VERSION "1.0"
